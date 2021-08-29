194a087e42f2461adace2fd28042c0f6c72c3efd
Taras <taras_hvil@mail.ru>
2021-08-29 20:49:39 +0300

    added mongo config

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { TopPageModel } from './top-page.model';
 import { FindTopPageDto } from './dto/find-top-page.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('top-page')
 export class TopPageController {
